8659592e94372e660e03b6e539550c1fba1ea994
Rydhlnst <rydhlnst@gmail.com>
2025-03-11 03:09:02 +0700

    Mobile Navbar

--- components/navigation/navbar/Navbar.tsx
+++ components/navigation/navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { Theme } from './Theme'
+import MobileNavbar from './MobileNavbar'
 
 const Navbar = () => {
   return (
@@ -15,6 +16,8 @@ const Navbar = () => {
         <p>Global Search</p>
         <div className='flex-between gap-5'>
             <Theme/>
+
+            <MobileNavbar/>
         </div>
     </nav>
   )
---
+++ constants/index.ts
@@ -0,0 +1,37 @@
+export const sidebarLinks = [
+    {
+        imgUrl: "/icons/home.svg",
+        label: "Home",
+        route: "/"
+    },
+    {
+        imgUrl: "/icons/users.svg",
+        label: "Community",
+        route: "/community"
+    },
+    {
+        imgUrl: "/icons/star.svg",
+        label: "Collections",
+        route: "/collection"
+    },
+    {
+        imgUrl: "/icons/suitcase.svg",
+        label: "Find Jobs",
+        route: "/jobs"
+    },
+    {
+        imgUrl: "/icons/tag.svg",
+        label: "Tags",
+        route: "/tag"
+    },
+    {
+        imgUrl: "/icons/user.svg",
+        label: "Profile",
+        route: "/user"
+    },
+    {
+        imgUrl: "/icons/question.svg",
+        label: "Ask a question",
+        route: "/ask-question"
+    }
+   ]
